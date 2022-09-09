964812d2ed54d949b4d773d4f3ba63cf44ffd9c2
Jay Jonkman <jayjonk@gmail.com>
2022-09-09 17:05:46 +0200

    added comments

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import masscan
 from mcstatus import JavaServer
 import random
 
+#masscan rate
 maxrate = 250
 
+#set ip range
 first = random.randint(0,255)
 second = random.randint(0,255)
 first = 85
 second = 214
-
 iprange = str(first) + '.' + str(second) + ".0.0/16"
 print(iprange)
 
-
+#scan ip range for port 25565
 mas = masscan.PortScanner()
 try:
     mas.scan(iprange, ports='25565', arguments=('--max-rate ' + str(maxrate) + " --excludefile exclude.conf"))
@@ -25,16 +26,25 @@ except Exception as e:
 
 else:
     for ip in ips:
+        #get server info
         try:
             server = JavaServer.lookup(ip)
             status = server.status().raw
+            max = int(status["players"]["max"])
+            version = status["version"]["name"]
+            onlinenr = int(status["players"]["online"])
+            if onlinenr > 0:
+                samplepl = status["players"]["sample"]
+        #catch exception
         except Exception as e:
             print('\n')
             print(ip + ": " + str(e))
+        #prints info
         else:
             print('\n')
             print(ip)
-            print(status)
-            if int(status["players"]["online"]) > 0:
-                for player in status["players"]["sample"]:
+            print(str(onlinenr) + "/" + str(max))
+            print(version)
+            if int(onlinenr) > 0:
+                for player in samplepl:
                     print(player)
